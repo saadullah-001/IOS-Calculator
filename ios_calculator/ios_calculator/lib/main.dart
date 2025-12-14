8b759fae71d39b780e217683a439a355578b54de
SaadUllah <saadullah.developers@gmail.com>
2025-12-15 04:43:34 +0500

    correcting code

--- ios_calculator/ios_calculator/lib/main.dart
+++ ios_calculator/ios_calculator/lib/main.dart
@@ -1,20 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:ios_calculator/ui.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'ui.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CalculatorScreen(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String input = "0";
+  String result = "0";
+
+  final Color orange = const Color(0xFFFF9500);
+  final Color lightGrey = const Color(0xFFA5A5A5);
+  final Color darkGrey = const Color(0xFF333333);
+
+  void buttonPressed(String btnText) {
+    setState(() {
+      if (btnText == "AC") {
+        input = "0";
+        result = "0";
+      } else if (btnText == "⌫") {
+        if (input.isNotEmpty && input != "0") {
+          input = input.substring(0, input.length - 1);
+          if (input.isEmpty) input = "0";
+        }
+      } else if (btnText == "=") {
+        try {
+          String expression = input.replaceAll('x', '*').replaceAll('÷', '/');
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+          ContextModel cm = ContextModel();
+          double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+          result = eval.toString();
+          if (result.endsWith(".0")) {
+            result = result.substring(0, result.length - 2);
+          }
+          input = result;
+        } catch (e) {
+          result = "Error";
+        }
+      } else {
+        if (input == "0") {
+          input = btnText;
+        } else {
+          input += btnText;
+        }
+      }
+    });
+  }
+
+  // Helper widget to make code cleaner
+  Widget buildButtonRow(List<Widget> buttons) {
+    return Expanded(
+      child: Row(
+        crossAxisAlignment:
+            CrossAxisAlignment.stretch, // Makes buttons fill height
+        children: buttons,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: UI());
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        // Protects from notches/home bars
+        child: Column(
+          children: [
+            // Display Area
+            display(input, result),
+
+            // Keypad
+            buildButtonRow([
+              Button(
+                str: "AC",
+                colors: lightGrey,
+                textColor: Colors.black,
+                onTap: () => buttonPressed("AC"),
+              ),
+              Button(
+                str: "⌫",
+                colors: lightGrey,
+                textColor: Colors.black,
+                onTap: () => buttonPressed("⌫"),
+              ),
+              Button(
+                str: "%",
+                colors: lightGrey,
+                textColor: Colors.black,
+                onTap: () => buttonPressed("%"),
+              ),
+              Button(str: "÷", colors: orange, onTap: () => buttonPressed("÷")),
+            ]),
+            buildButtonRow([
+              Button(str: "7", onTap: () => buttonPressed("7")),
+              Button(str: "8", onTap: () => buttonPressed("8")),
+              Button(str: "9", onTap: () => buttonPressed("9")),
+              Button(str: "x", colors: orange, onTap: () => buttonPressed("x")),
+            ]),
+            buildButtonRow([
+              Button(str: "4", onTap: () => buttonPressed("4")),
+              Button(str: "5", onTap: () => buttonPressed("5")),
+              Button(str: "6", onTap: () => buttonPressed("6")),
+              Button(str: "-", colors: orange, onTap: () => buttonPressed("-")),
+            ]),
+            buildButtonRow([
+              Button(str: "1", onTap: () => buttonPressed("1")),
+              Button(str: "2", onTap: () => buttonPressed("2")),
+              Button(str: "3", onTap: () => buttonPressed("3")),
+              Button(str: "+", colors: orange, onTap: () => buttonPressed("+")),
+            ]),
+            buildButtonRow([
+              specialButton(() => buttonPressed("0")),
+              Button(str: ".", onTap: () => buttonPressed(".")),
+              Button(str: "=", colors: orange, onTap: () => buttonPressed("=")),
+            ]),
+          ],
+        ),
+      ),
+    );
   }
 }
--- ios_calculator/ios_calculator/lib/ui.dart
+++ ios_calculator/ios_calculator/lib/ui.dart
@@ -1,208 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:ios_calculator/const.dart';
-import 'package:ios_calculator/widgets.dart';
-import 'package:math_expressions/math_expressions.dart';
 
-String displayNum = '';
-String result = '';
+// Colors
+const Color darkGrey = Color(0xFF333333);
+const Color white = Colors.white;
 
-class UI extends StatefulWidget {
-  const UI({super.key});
+class Button extends StatelessWidget {
+  final Color colors;
+  final String str;
+  final Color textColor;
+  final VoidCallback onTap;
 
-  @override
-  State<UI> createState() => _UIState();
-}
+  const Button({
+    super.key,
+    this.colors = darkGrey,
+    required this.str,
+    this.textColor = white,
+    required this.onTap,
+  });
 
-class _UIState extends State<UI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: black,
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            display(),
-            Expanded(
-              flex: 2,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Button(
-                        colors: lightGrey,
-                        textColor: black,
-                        str: 'AC',
-                        func: () {
-                          displayNum = '';
-                          result = '';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: lightGrey,
-                        textColor: black,
-                        str: '+/-',
-                        func: () {
-                          displayNum += '+/-';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: lightGrey,
-                        textColor: black,
-                        str: '%',
-                        func: () {
-                          displayNum += '%';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: orange,
-                        str: '/',
-                        func: () {
-                          displayNum += '/';
-                          setState(() {});
-                        },
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Button(
-                        str: '7',
-                        func: () {
-                          displayNum += '7';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '8',
-                        func: () {
-                          displayNum += '8';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '9',
-                        func: () {
-                          displayNum += '9';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: orange,
-                        str: '*',
-                        func: () {
-                          displayNum += '*';
-                          setState(() {});
-                        },
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Button(
-                        str: '4',
-                        func: () {
-                          displayNum += '4';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '5',
-                        func: () {
-                          displayNum += '5';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '6',
-                        func: () {
-                          displayNum += '6';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: orange,
-                        str: '-',
-                        func: () {
-                          displayNum += '-';
-                          setState(() {});
-                        },
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Button(
-                        str: '1',
-                        func: () {
-                          displayNum += '1';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '2',
-                        func: () {
-                          displayNum += '2';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        str: '3',
-                        func: () {
-                          displayNum += '3';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: orange,
-                        str: '+',
-                        func: () {
-                          displayNum += '+';
-                          setState(() {});
-                        },
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      specialButton(() {
-                        setState(() {
-                          displayNum += '0';
-                        });
-                      }),
-
-                      Button(
-                        str: '.',
-                        func: () {
-                          displayNum += '.';
-                          setState(() {});
-                        },
-                      ),
-                      Button(
-                        colors: orange,
-                        str: '=',
-                        func: () {
-                          Parser p = Parser();
-                          Expression exp = p.parse(displayNum);
-                          ContextModel cm = ContextModel();
-                          result = exp
-                              .evaluate(EvaluationType.REAL, cm)
-                              .toString();
-
-                          setState(() {});
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(6.0), // Adds gap between buttons
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(100),
+          child: Container(
+            decoration: BoxDecoration(color: colors, shape: BoxShape.circle),
+            child: Center(
+              child: FittedBox(
+                // Scales text to fit the button
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  str,
+                  style: TextStyle(fontSize: 32, color: textColor),
+                ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+Expanded display(String input, String result) {
+  return Expanded(
+    flex: 2, // Gives the display 2x more space than a single button row
+    child: Container(
+      width: double.infinity,
+      alignment: Alignment.bottomRight,
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          // Input Text (Scales automatically)
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: Alignment.centerRight,
+            child: Text(
+              input,
+              style: const TextStyle(
+                fontSize: 64, // Large base size, will shrink if needed
+                color: white,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Result Text
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: Alignment.centerRight,
+            child: Text(
+              result,
+              style: const TextStyle(fontSize: 30, color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+// Special "0" Button (Wide Button)
+Widget specialButton(VoidCallback onTap) {
+  return Expanded(
+    flex: 2, // Takes up the width of 2 buttons
+    child: Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(40),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.only(left: 30),
+          decoration: BoxDecoration(
+            color: darkGrey,
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: const Text('0', style: TextStyle(fontSize: 32, color: white)),
+        ),
+      ),
+    ),
+  );
 }
